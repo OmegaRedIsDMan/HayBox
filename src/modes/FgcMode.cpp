bc14e32315641a82fc5d936b7831a7b13a909e0b
CORE-I7-QUAD\DMan <derekh17@hotmail.com>
2023-08-13 21:10:17 -0500

    modified FGC mode button layout to work better for me personally on Street Figher 6

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -1,20 +1,21 @@
 #include "modes/FgcMode.hpp"
 
 FgcMode::FgcMode(socd::SocdType socd_type) : ControllerMode(socd_type) {
-    _socd_pair_count = 1;
+    _socd_pair_count = 2;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
         socd::SocdPair{&InputState::left, &InputState::right},
+        socd::SocdPair{&InputState::up, &InputState::down},
     };
 }
 
 void FgcMode::HandleSocd(InputState &inputs) {
     // dhall - I think the up inputs here are all the same as 
     // in the UpdateDigitalOutputs function below
-    if (inputs.down && (inputs.mod_x    || inputs.mod_y     ||
-                        inputs.mod_x2   || inputs.mod_y2    ||
-                        inputs.c_up     || inputs.c_down    ||
-                        inputs.c_left   || inputs.c_right   ||
-                        inputs.up       || inputs.a)) {
+    if (inputs.down && (inputs.mod_x    || /*inputs.mod_y     ||*/
+                        inputs.mod_x2   || /*inputs.mod_y2    ||*/
+                        /*inputs.c_up     ||*/ inputs.c_down    ||
+                        inputs.c_left   || /*inputs.c_right   ||*/
+                        /*inputs.up       ||*/ inputs.a)) {
         inputs.down = false;
     }
     InputMode::HandleSocd(inputs);
@@ -25,30 +26,34 @@ void FgcMode::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.dpadLeft = inputs.left;
     outputs.dpadRight = inputs.right;
     outputs.dpadDown = inputs.down;
-    outputs.dpadUp =    inputs.mod_x    || inputs.mod_y     ||
-                        inputs.mod_x2   || inputs.mod_y2    ||
+    outputs.dpadUp =    inputs.mod_x    || /*inputs.mod_y     ||*/
+                        inputs.mod_x2   || /*inputs.mod_y2    ||*/
                         /*inputs.c_up     ||*/ inputs.c_down    ||
                         inputs.c_left   || /*inputs.c_right   ||*/
                         /*inputs.up       ||*/ inputs.a;
 
     // Menu keys
     outputs.start = inputs.start;
-    //outputs.select = inputs.c_left;
-    //outputs.home = inputs.c_down;
+    outputs.select = inputs.up;//c_left;
+    //outputs.home = inputs.mod_y2;//c_down;
+
+    // Right hand top row
+    outputs.x = inputs.r;
+    outputs.y = inputs.y;
+    outputs.buttonL = inputs.midshield;
+    //outputs.buttonR = inputs.lightshield;
 
     // Right hand bottom row
     outputs.a = inputs.b;
     outputs.b = inputs.x;
     outputs.triggerRDigital = inputs.z;
     //outputs.triggerLDigital = inputs.up;
-    outputs.triggerLDigital = inputs.k4;
+    outputs.triggerLDigital = inputs.lightshield;//inputs.k4;
+
+    // c-stick buttons 
+    outputs.buttonR = inputs.c_up;
 
 
-    // Right hand top row
-    outputs.x = inputs.r;
-    outputs.y = inputs.y;
-    outputs.buttonR = inputs.lightshield;
-    outputs.buttonL = inputs.midshield;
 }
 
 void FgcMode::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
